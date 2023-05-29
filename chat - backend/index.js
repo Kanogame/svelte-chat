027d81602b4857436533715d379dc4c21b08fb1b
kanogame <wasdqert1222@gmail.com>
2023-05-29 19:58:42 +0700

    backend done

--- chat - backend/index.js
+++ chat - backend/index.js
@@ -1,13 +1,43 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParcser = require("body-parser");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
-app.use(bodyParcser.json());
+app.use(bodyParser.json());
 
+let messages = [
+  {
+    id: 1,
+    user: 'Марина',
+    text: 'Привет. Как дела?',
+  },
+  {
+    id: 2,
+    user: 'Павел',
+    text: 'Всё ок. Как сама?',
+  },
+];
+let maxId = 2;
 
+app.post('/api/messages_get', (req, res) => {
+  res.json(messages);
+});
 
-app.listen(13531, () => {
-    console.log("Сервер запущен на порту 13531");
+app.post('/api/message_add', (req, res) => {
+  const { user, text } = req.body;
+  maxId++;
+  messages.push({
+    id: maxId,
+    user,
+    text,
+  });
+  res.json({
+    success: true,
+  });
+});
+
+const port = 13531;
+app.listen(port, () => {
+  console.log(`Бекенд запущен на порту ${port}`);
 });
