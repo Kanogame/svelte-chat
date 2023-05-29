42a058ee3cc1c4bb26d94d05e9dfce893c239975
kanogame <wasdqert1222@gmail.com>
2023-05-29 20:36:57 +0700

    anti-spam

--- chat - backend/index.js
+++ chat - backend/index.js
@@ -20,11 +20,54 @@ let messages = [
 ];
 let maxId = 2;
 
+let maxRequests = 60;
+
+// minutes
+let period = 1;
+
+const blockedIps = {};
+// [ip]: <count_of_requests>
+const requests = {};
+
+function bad(req, res) {
+  // console.log('remoteaddr' + req.socket.remoteAddress);
+  // console.log('xforwarded' + req.headers['x-forwarded-for']);
+  const ip = req.socket.remoteAddress;
+  if (ip in blockedIps) {
+    res.json({
+      error: 'Too many requests',
+    });
+    return true;
+  }
+  if (!(ip in requests)) {
+    requests[ip] = 0;
+  }
+  requests[ip]++;
+  if (requests[ip] >= maxRequests) {
+    blockedIps[ip] = true;
+  }
+  console.log(requests);
+  setTimeout(() => {
+    if (ip in requests) {
+      requests[ip]--;
+      console.log(requests);
+      if (requests[ip] <= 0) {
+        delete requests[ip];
+      }
+    }
+  }, period * 60 * 1000);
+
+  return false;
+}
+
 app.post('/api/messages_get', (req, res) => {
   res.json(messages);
 });
 
 app.post('/api/message_add', (req, res) => {
+  if (bad(req, res)) {
+    return;
+  }
   const { user, text } = req.body;
   maxId++;
   messages.push({
